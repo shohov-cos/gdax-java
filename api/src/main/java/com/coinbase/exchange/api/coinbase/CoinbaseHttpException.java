9bcaeefb7cb62d49c5a1e3661cf4fc30896d6d2c
Dmitry Shohov <d.shohovs@cospay.com>
2021-09-18 09:38:13 +0300

    constructor in CoinbaseHttpException for easy mocking

--- api/src/main/java/com/coinbase/exchange/api/coinbase/CoinbaseHttpException.java
+++ api/src/main/java/com/coinbase/exchange/api/coinbase/CoinbaseHttpException.java
@@ -1,21 +1,46 @@
 package com.coinbase.exchange.api.coinbase;
 
+import java.net.http.HttpHeaders;
 import java.net.http.HttpResponse;
 
 public class CoinbaseHttpException extends CoinbaseException {
-
+    private final int statusCode;
+    private final String body;
+    private final HttpHeaders headers;
     private final HttpResponse<String> response;
 
     public CoinbaseHttpException(HttpResponse<String> response) {
         super("Request failed with status code: " + response.statusCode() + ", body: " + response.body());
+        this.statusCode = response.statusCode();
+        this.body = response.body();
+        this.headers = response.headers();
         this.response = response;
+    }
+
+    /**
+     * Can be used for mocking. Response will return null with this constructor
+     */
+    public CoinbaseHttpException(int statusCode, String body, HttpHeaders headers) {
+        super("Request failed with status code: " + statusCode + ", body: " + body);
+        this.statusCode = statusCode;
+        this.body = body;
+        this.headers = headers;
+        this.response = null;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public HttpHeaders getHeaders() {
+        return headers;
     }
 
     public HttpResponse<String> getResponse() {
         return response;
-    }
-
-    public String getBody() {
-        return response.body();
     }
 }
